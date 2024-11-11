df22b66aa5a1fabe68024daf42e760046e57d91e
Marcela Gonzaga <mgonzaga@c1r5p3.42sp.org.br>
2024-11-11 19:39:50 -0300

    ka

---
+++ cpp00/ex01/contact.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgonzaga <mgonzaga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/11 17:02:43 by mgonzaga          #+#    #+#             */
+/*   Updated: 2024/11/11 19:37:39 by mgonzaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact{
+	private:
+		std::string first_Name;
+		std::string	last_Name;
+		std::string	nick_Name;
+		std::string	phoneNumber;
+		std::string	darkestSecret;
+	
+	public:
+		
+
+
+}
