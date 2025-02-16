5e2e1db43c4453b0cf459c9e525606bfb2f622c7
Marcela Gonzaga <134085407+mgonzaga11@users.noreply.github.com>
2025-02-16 19:35:46 +0000

    ex02

---
+++ cpp01/ex02.cpp
@@ -0,0 +1,20 @@
+
+#include <ostream>
+#include <string>
+#include <iostream>
+
+int	main(void) {
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*strPTR = &str;
+	std::string	&strREF = str;
+
+	std::cout << "The memory address: " << &str << std::endl;
+	std::cout << "The memory address: " << strPTR << std::endl;
+	std::cout << "The memory address: " << &strREF << std::endl;
+
+	std::cout << "The value pointed: " << str << std::endl;
+	std::cout << "The value pointed: " << *strPTR << std::endl;
+	std::cout << "The value pointed: " << strREF << std::endl;
+
+	return (0);
+}
