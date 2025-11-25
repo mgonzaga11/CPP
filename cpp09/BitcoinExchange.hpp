880772502376bc75e67825c8f7a8058e2003c587
Marcela Gonzaga <134085407+mgonzaga11@users.noreply.github.com>
2025-11-25 22:56:35 +0000

    as

---
+++ cpp09/BitcoinExchange.hpp
@@ -0,0 +1,33 @@
+
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+# include <map>
+# include <string>
+
+class	BitcoinExchange {
+
+private:
+	std::map<std::string, float>	_data;
+	std::string						_file;
+
+	BitcoinExchange(void);
+	BitcoinExchange(BitcoinExchange const &param);
+
+	BitcoinExchange	&operator=(BitcoinExchange const &param);
+
+	void	setData(void);
+	bool	validateDate(std::string const &date) const;
+	float	convertBitcoin(std::string const &date, float const &valueInput);
+	void	printExchange(std::string const &input);
+	bool	validateInput(std::string const &input) const;
+	float	findValue(std::string const &key) const;
+	void	printData(void) const;
+
+public:
+	~BitcoinExchange(void);
+	BitcoinExchange(std::string const &file);
+
+	void	printFile(std::string const &file);
+};
+
+#endif
